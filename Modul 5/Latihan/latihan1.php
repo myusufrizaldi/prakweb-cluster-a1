db25c5556c55fcf2193de9f7a4061b56424a12b9
Yusuf <mohamadyusufrizaldi@gmail.com>
2020-11-11 00:24:42 +0700

    Menambahkan latihan Modul 5

---
+++ Modul 5/Latihan/latihan1.php
@@ -0,0 +1,37 @@
+<?php
+    $a = 20;
+    $b = 5;
+
+    $addition = $a + $b;
+    $substraction = $a - $b;
+    $multiplication = $a * $b;
+    $division = $a / $b;
+    $mod = $a % $b;
+
+    echo 'Bilangan 1 = ' . $a . '<br>';
+    echo 'Bilangan 2 = ' . $b . '<br>';
+    echo '<br>';
+    echo 'Berikut merupakan hasil dari setiap operasi<br>';
+    echo '<br>';
+    echo 'PENJUMLAHAN<br>';
+    echo 'Operator : +<br>';
+    echo 'Hasil    : ' . $addition . '<br>';
+    echo '<br>';
+    echo 'PENGURANGAN<br>';
+    echo 'Operator : -<br>';
+    echo 'Hasil    : ' . $substraction . '<br>';
+    echo '<br>';
+    echo 'PERKALIAN<br>';
+    echo 'Operator : *<br>';
+    echo 'Hasil    : ' . $multiplication . '<br>';
+    echo '<br>';
+    echo 'PEMBAGIAN<br>';
+    echo 'Operator : /<br>';
+    echo 'Hasil    : ' . $division . '<br>';
+    echo '<br>';
+    echo 'MODULUS<br>';
+    echo 'Operator : %<br>';
+    echo 'Hasil    : ' . $mod . '<br>';
+    echo '<br>';
+
+?>
---
+++ Modul 5/Latihan/latihan2.php
@@ -0,0 +1,10 @@
+<?php
+
+$arr = array('dino', 'alit', 'febriyanto', 'maldini', 'abdillah', 'irfandi', 'ambar', 'winarsih', 'yusuf', 'rizaldi');
+sort($arr);
+
+foreach($arr as $elm) {
+    echo $elm . '<br>';
+}
+
+?>
